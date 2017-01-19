d5215f09766cd6e44374791d3b235719004ea480
Issayev Adilet <issayev.adilet@gmail.com>
2017-01-19 19:02:23 +0600

    Added test comment

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -127,3 +127,5 @@ Route::get('api/requestexitmatch/{matchid}/{userid}','ApiController@requestexitm
 Route::get('api/requestweekdays/{playgroundid}/{selecteddate}','ApiController@requestweekdays');
 Route::post('api/requestmatchcreate/','ApiController@requestmatchcreate');
 Route::post('api/requestsign64/','ApiController@requestsign64');
+
+// Test
